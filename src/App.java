cb5310aa969fbd4b0fd3136dc31f639b3054f4d3
carlosdtna <carlosdetenamunoz@gmail.com>
2025-04-08 19:56:55 +0200

    Update App.java

--- src/App.java
+++ src/App.java
@@ -2,12 +2,9 @@ import java.io.File; //Este import sirve para poder utilizar la clase File.
 import java.io.IOException; //Este import sirve para poder utilizar la excepcion IO porque estamos utilizando java.io (es el paquete que se usa para fichero :).
 
 /**
-* Creación de un controlador (se encarga de crear la vista y el modelo; y de cambiar las vista), el modelo vista-controlador
-* se utiliza para mantener la independencia del código y así el código es más escalable(que se puede añadir más cosas),
-* mantenible, y permite reutilizar código.
-* El modelo es una clase(para crear los objetos jugadores y escenarios)
-* @author Carlos de Tena
-* @author Paloma Zapata
+* Esta clase es la que tiene el main en el cual se inicia el juego a través de un controlador también creado en el main.
+* @author Carlos de Tena Muñoz
+* @author Paloma Zapata Velázquez
 * @version 2.0
 */
 public class App {
